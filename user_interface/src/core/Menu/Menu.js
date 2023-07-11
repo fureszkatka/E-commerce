c04aa24abdf6ec3bdb2304c8725d135cb313fd87
manjaro-kati <fureszkatka@gmail.com>
2023-07-11 22:02:42 +0200

    check order render or no render

--- user_interface/src/core/Menu/Menu.js
+++ user_interface/src/core/Menu/Menu.js
@@ -63,11 +63,15 @@ class Menu extends Component {
                     </div>
                 </div>
                 <div className='Menu_headline'>
-                <Link className='Menu_kate_style' to = "/">Kate style</Link>
+                    <Link className='Menu_kate_style' to = "/">Kate style</Link>
                     <input className ="Menu_searchbar"value = {this.state.search} onChange={(e)=>this.change(e)}/>
                     <SearchOutlined onClick={this.search} className='Menu_search-button'></SearchOutlined>
-                    <Link to ={this.props.auth.isLoggedIn ? `/${this.props.auth.user.id}/cart` : '/login'} className='Menu_cart'>Cart</Link>
-                    <Link className='Menu_cart' to={`/order/${this.props.auth.user.id}`}>Check order</Link>
+                    {  this.props.auth.isLoggedIn && 
+                        <>
+                        <Link to ={this.props.auth.isLoggedIn ? `/${this.props.auth.user.id}/cart` : '/login'} className='Menu_cart'>Cart</Link>
+                        <Link className='Menu_cart' to={`/order/${this.props.auth.user.id}`}>Check order</Link>
+                        </>
+                    }
                 </div>
             </div>
         )
